dd65ed8c4be4ce12ed9bc33728c55912cc3aff70
Safturento <safturento@gmail.com>
2017-10-12 15:11:49 -0500

    add reciever RS-232

--- reciever.py
+++ reciever.py
@@ -11,24 +11,44 @@ ledInput = []
 
 io.setup(iLED, io.IN)
 
-start = time.time()
+start = 0
 
-while time.time()-start < 10:
-	time.sleep(dt)
-	ledInput.append(io.input(iLED))
+def toChar(bits):
+	return chr(int(''.join(bits),2))
 
-p = re.compile("([1]+[0]+)")
 
-#splits input into chunks of 1's followed by 0's
-splitInput = list(filter(None, p.split(''.join([str(x) for x in ledInput]))[1:]))
+while True:
+	# Possible start bit
+	if io.input(iLED):
+		time.sleep(dt*.5)
+		# Check if valid start bit
+		if io.input(iLED):
+			bits = []
+			for i in range(8):
+				time.sleep(dt)
+				bits.append(io.input(iLED))
+			print(toChar(bits) ,end='')
 
-#print(len(splitBits))
 
-parsedBits = []
-for x in splitInput:
-	print(1 if x.count('1')/len(x) > .5 else 0)
-	parsedBits.append(1 if x.count('1')/len(x) > .5 else 0)
 
-recievedStr = ''.join([chr(int(''.join(x),2)) for x in zip(*[iter(''.join(map(str,parsedBits)))]*8)])
 
-print(recievedStr)
+
+# while time.time()-start < 10:
+# 	time.sleep(dt)
+# 	ledInput.append(io.input(iLED))
+
+# p = re.compile("([1]+[0]+)")
+
+# #splits input into chunks of 1's followed by 0's
+# splitInput = list(filter(None, p.split(''.join([str(x) for x in ledInput]))[1:]))
+
+# #print(len(splitBits))
+
+# parsedBits = []
+# for x in splitInput:
+# 	print(1 if x.count('1')/len(x) > .5 else 0)
+# 	parsedBits.append(1 if x.count('1')/len(x) > .5 else 0)
+
+# recievedStr = ''.join([chr(int(''.join(x),2)) for x in zip(*[iter(''.join(map(str,parsedBits)))]*8)])
+
+# print(recievedStr)
